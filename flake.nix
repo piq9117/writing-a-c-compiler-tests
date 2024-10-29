40b5b70aaf2eb448d7f903a59f9e8c5c3b750bb9
piq9117 <piq9117@gmail.com>
2024-10-28 23:05:54 -0400

    flakes

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Basic haskell cabal template";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = final: prev: {
+        hsPkgs = prev.haskell.packages.ghc965.override {
+          overrides = hfinal: hprev: { };
+        };
+        init-project = final.writeScriptBin "init-project" ''
+          ${final.hsPkgs.cabal-install}/bin/cabal init --non-interactive
+        '';
+      };
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          libs = with pkgs; [
+            zlib
+            gcc
+            gdb
+            python3
+          ];
+        in
+        {
+          default = pkgs.hsPkgs.shellFor {
+            packages = hsPkgs: [ ];
+            buildInputs = with pkgs; [
+              hsPkgs.cabal-install
+              hsPkgs.cabal-fmt
+              hsPkgs.ghc
+              ormolu
+              treefmt
+              nixpkgs-fmt
+              hsPkgs.cabal-fmt
+              init-project
+            ] ++ libs;
+            shellHook = "export PS1='[$PWD]\n❄ '";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+          };
+        });
+    };
+}
