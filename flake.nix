b34a4234b4819420932018acd762a48dca3af010
piq9117 <piq9117@gmail.com>
2024-11-14 14:02:41 -0500

    batman

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Basic haskell cabal template";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = final: prev: {
+        hsPkgs = prev.haskell.packages.ghc965.override {
+          overrides = hfinal: hprev: { };
+        };
+      };
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          libs = with pkgs; [
+            zlib
+          ];
+        in
+        {
+          default = pkgs.hsPkgs.shellFor {
+            packages = hsPkgs: [ ];
+            buildInputs = with pkgs; [
+              hsPkgs.cabal-install
+              hsPkgs.cabal-fmt
+              hsPkgs.ghc
+              ormolu
+              treefmt
+              nixpkgs-fmt
+              hsPkgs.cabal-fmt
+            ] ++ libs;
+            shellHook = "export PS1='[$PWD]\n❄ '";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+          };
+        });
+    };
+}
