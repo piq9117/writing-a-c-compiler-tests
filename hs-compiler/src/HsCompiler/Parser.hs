6afed3a5d572021d7e2756d1e9d4026db64e6e0b
piq9117 <piq9117@gmail.com>
2024-12-19 20:50:25 -0600

    more parsers

--- hs-compiler/src/HsCompiler/Parser.hs
+++ hs-compiler/src/HsCompiler/Parser.hs
@@ -3,6 +3,14 @@
 module HsCompiler.Parser
   ( keyword,
     keywords,
+    identifier,
+    openParens,
+    closeParens,
+    openBrace,
+    closeBrace,
+    semicolon,
+    constant,
+    Parser,
   )
 where
 
@@ -21,3 +29,38 @@ keyword =
 
 keywords :: [Text]
 keywords = ["int", "void", "return"]
+
+identifier :: Parser Text
+identifier =
+  fmap toText $
+    many identifierStart
+  where
+    identifierStart = do
+      -- Text.Megaparsec.notFollowedBy Text.Megaparsec.Char.digitChar
+      Text.Megaparsec.Char.asciiChar
+        <|> Text.Megaparsec.Char.upperChar
+        <|> Text.Megaparsec.Char.lowerChar
+        <|> Text.Megaparsec.Char.char '_'
+
+openParens :: Parser Text
+openParens =
+  toText <$> Text.Megaparsec.Char.string "("
+
+closeParens :: Parser Text
+closeParens =
+  toText <$> Text.Megaparsec.Char.string ")"
+
+openBrace :: Parser Text
+openBrace =
+  toText <$> (Text.Megaparsec.Char.string "{")
+
+closeBrace :: Parser Text
+closeBrace =
+  toText <$> (Text.Megaparsec.Char.string "}")
+
+semicolon :: Parser Text
+semicolon =
+  toText <$> (Text.Megaparsec.Char.string ";")
+
+constant :: Parser Text
+constant = toText <$> many Text.Megaparsec.Char.digitChar
--- hs-compiler/test/Spec/Parser.hs
+++ hs-compiler/test/Spec/Parser.hs
@@ -15,12 +15,38 @@ import Text.Megaparsec.Char qualified
 
 parser :: Spec
 parser = describe "Parsers" $ do
-  it "keyword" $
+  it "keywords" $
     Text.Megaparsec.parse
       (many (HsCompiler.Parser.keyword <* Text.Megaparsec.Char.eol))
       "test"
       (unlines HsCompiler.Parser.keywords)
       `shouldBe` (Right ["int", "void", "return"])
+
+  it "identifier" $ do
+    Text.Megaparsec.parse
+      HsCompiler.Parser.identifier
+      "test"
+      "deeznuts"
+      `shouldBe` (Right "deeznuts")
+
+    Text.Megaparsec.parse
+      HsCompiler.Parser.identifier
+      "test"
+      "deez_nuts"
+      `shouldBe` (Right "deez_nuts")
+
+    Text.Megaparsec.parse
+      HsCompiler.Parser.identifier
+      -- ((fmap toText $ many Text.Megaparsec.Char.asciiChar) :: HsCompiler.Parser.Parser Text)
+      "test"
+      "deezNuts"
+      `shouldBe` (Right "deezNuts")
+
+    Text.Megaparsec.parse
+      HsCompiler.Parser.identifier
+      "test"
+      "1"
+      `shouldBe` (Right "deezNuts")
 
 test_testTree :: IO TestTree
 test_testTree =
